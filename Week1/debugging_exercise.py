2fdb7b655ced1a3c9299eb047fd29b2046c3a190
O-2me <129533730+O-2me@users.noreply.github.com>
2023-04-05 12:56:40 +0100

    Update debugging_exercise.py
    Count feature added

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -2,11 +2,14 @@ import random
 
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
+# number of guesses made by the user
+count = 1
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
+    count += 1
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
@@ -16,3 +19,4 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     guess = int(input())
 
 print('You got it! The number was {}'.format(rand))
+print('You did it in {} tries'.format(count))
